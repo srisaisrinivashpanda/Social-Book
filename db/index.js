e57683dc854de787e0877484ffaeefeabc460dba
srinivash <unitofsrinivash@gmail.com>
2026-06-11 02:16:19 +0530

    feat: add express server setup with middleware and database connection

--- db/index.js
+++ db/index.js
@@ -1,2 +1,18 @@
-import { DB_NAME } from '../src/constants.js';
-import mongoose from 'mongoose';
+import { DB_NAME } from "../src/constants.js";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MONGODB Connected !! DB Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection failed: ", error);
+    throw error;
+  }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import dns from "node:dns/promises";
+
+import connectDB from "../db.js";
+import app from "./app.js";
 
 dotenv.config({
-   path: '.env',
+  path: ".env",
 });
 
-//DNS import
-import dns from 'node:dns/promises';
-dns.setServers(['1.1.1.1']);
+dns.setServers(["1.1.1.1"]);
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
