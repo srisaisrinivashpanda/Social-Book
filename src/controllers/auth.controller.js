db1d71767843f0321cc9100f47952b0186aa269b
srinivash <unitofsrinivash@gmail.com>
2026-06-14 21:26:52 +0530

    feat: export refreshAccessToken function from auth.controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -229,4 +229,4 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, loginUser, logoutUser };
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
